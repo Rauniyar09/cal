c51a0ccf06a962746451c19e13185e6c1d74dadb
Rahul Rauniyar <69071433+stepin104907@users.noreply.github.com>
2020-10-07 23:33:30 +0530

    Add files via upload

---
+++ calculator/inc/cal.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//function declarations
+void display(float n1, float n2, char ch, float result);
+float add(float n1, float n2);
+float subtract(float n1, float n2);
+float multiply(float n1, float n2);
+float divide(float n1, float n2);
+float rem(float n1, float n2);
+float power(float n1, float n2);
